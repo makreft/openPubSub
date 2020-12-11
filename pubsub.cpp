a44ad1005141b375c6498706350d6e31d06a7b1c
Marco Kreft <marco.kreft@tu-braunschweig.de>
2020-12-12 00:15:34 +0100

    add pubsub and sub from sinans code

---
+++ pubsub.cpp
@@ -0,0 +1,100 @@
+//
+// Created by sba on 16.09.19.
+//
+
+
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+#include <open62541/plugin/pubsub.h>
+#include <open62541/plugin/pubsub_udp.h>
+#include <open62541/plugin/log.h>
+#include <open62541/plugin/log_stdout.h>
+#include <signal.h>
+#include <stdio.h>
+#include <open62541/server_pubsub.h>
+#include <open62541/types_generated.h>
+#include <open62541/types.h>
+
+UA_Boolean running = true;
+
+static void stopHandler(int sign) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+    running = false;
+}
+
+int main(int argc, char **argv) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    UA_Server *server = UA_Server_new();
+    UA_ServerConfig *config = UA_Server_getConfig(server);
+    UA_ServerConfig_setDefault(config);
+
+    UA_NodeId connectionID;
+    UA_NodeId pdsID;
+    UA_NodeId dataSetFieldID;
+    UA_NodeId dataSetWriterID;
+    UA_NodeId writerGroupID;
+
+    config->pubsubTransportLayers = (UA_PubSubTransportLayer *) UA_malloc(sizeof(UA_PubSubTransportLayer));
+    config->pubsubTransportLayers[0] = UA_PubSubTransportLayerUDPMP();
+    config->pubsubTransportLayersSize++;
+
+    UA_PubSubConnectionConfig connectionConfig;
+    memset(&connectionConfig, 0, sizeof(connectionConfig));
+    connectionConfig.name = UA_STRING("Demo UADP CON");
+    connectionConfig.transportProfileUri = UA_STRING("http://opcfoundation.org/UA-Profile/Transport/pubsub-udp-uadp");
+    connectionConfig.enabled = UA_TRUE;
+    UA_NetworkAddressUrlDataType networkAddressUrl = {UA_STRING_NULL , UA_STRING("opc.udp://127.0.0.1:4840/")};
+    UA_Variant_setScalar(&connectionConfig.address, &networkAddressUrl,&UA_TYPES[UA_TYPES_NETWORKADDRESSURLDATATYPE]);
+    connectionConfig.publisherId.numeric = 2234;
+    UA_Server_addPubSubConnection(server, &connectionConfig, &connectionID);
+
+    UA_PublishedDataSetConfig publishedDataSetConfig;
+    memset(&publishedDataSetConfig, 0, sizeof(UA_PublishedDataSetConfig));
+    publishedDataSetConfig.publishedDataSetType = UA_PUBSUB_DATASET_PUBLISHEDITEMS;
+    publishedDataSetConfig.name = UA_STRING("Demo PDS");
+    UA_Server_addPublishedDataSet(server, &publishedDataSetConfig, &pdsID);
+
+    UA_DataSetFieldConfig dataSetFieldConfig;
+    memset(&dataSetFieldConfig, 0, sizeof(dataSetFieldConfig));
+    dataSetFieldConfig.dataSetFieldType = UA_PUBSUB_DATASETFIELD_VARIABLE;
+    dataSetFieldConfig.field.variable.fieldNameAlias = UA_STRING("Demo Server localtime");
+    dataSetFieldConfig.field.variable.promotedField = UA_FALSE;
+    dataSetFieldConfig.field.variable.publishParameters.publishedVariable = UA_NODEID_NUMERIC(0,UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+    dataSetFieldConfig.field.variable.publishParameters.attributeId = UA_ATTRIBUTEID_VALUE;
+    UA_Server_addDataSetField(server, pdsID, &dataSetFieldConfig, &dataSetFieldID);
+
+    UA_WriterGroupConfig writerGroupConfig;
+    memset(&writerGroupConfig, 0, sizeof(writerGroupConfig));
+    writerGroupConfig.name = UA_STRING("Demo writergroup");
+    writerGroupConfig.publishingInterval = 100;
+    writerGroupConfig.enabled = UA_FALSE;
+    writerGroupConfig.writerGroupId = 100;
+    writerGroupConfig.encodingMimeType = UA_PUBSUB_ENCODING_UADP;
+    writerGroupConfig.messageSettings.encoding = UA_EXTENSIONOBJECT_DECODED;
+    writerGroupConfig.messageSettings.content.decoded.type = &UA_TYPES[UA_TYPES_UADPWRITERGROUPMESSAGEDATATYPE];
+    UA_UadpWriterGroupMessageDataType *writerGroupMessage = UA_UadpWriterGroupMessageDataType_new();
+    writerGroupMessage->networkMessageContentMask =
+            (UA_UadpNetworkMessageContentMask) (UA_UADPNETWORKMESSAGECONTENTMASK_PUBLISHERID   |
+            (UA_UadpNetworkMessageContentMask)  UA_UADPNETWORKMESSAGECONTENTMASK_GROUPHEADER   |
+            (UA_UadpNetworkMessageContentMask)  UA_UADPNETWORKMESSAGECONTENTMASK_WRITERGROUPID |
+            (UA_UadpNetworkMessageContentMask)  UA_UADPNETWORKMESSAGECONTENTMASK_PAYLOADHEADER );
+    writerGroupConfig.messageSettings.content.decoded.data = writerGroupMessage;
+    UA_Server_addWriterGroup(server, connectionID, &writerGroupConfig, &writerGroupID);
+    UA_UadpWriterGroupMessageDataType_delete(writerGroupMessage);
+
+    UA_DataSetWriterConfig dataSetWriterConfig;
+    memset(&dataSetWriterConfig, 0, sizeof(dataSetWriterConfig));
+    dataSetWriterConfig.name = UA_STRING("Demo Datasetwriter");
+    dataSetWriterConfig.dataSetWriterId = 62541;
+    dataSetWriterConfig.keyFrameCount = 10;
+    UA_Server_addDataSetWriter(server, writerGroupID, pdsID, &dataSetWriterConfig, &dataSetWriterID);
+
+    UA_StatusCode retval;
+    retval = UA_Server_run(server, &running);
+
+    UA_Server_delete(server);
+    return (int) retval;
+}
+
