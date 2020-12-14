6f15a6864e2bf950ea7b9ee70b6fad995a3f86f3
makreft <47369576+makreft@users.noreply.github.com>
2020-12-14 19:53:51 +0000

    Deploying to gh-pages from  @ 212471243e72ff66a5c4cc18320aea8063fe6984 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['server_6',['Server',['../classopenPubSub_1_1Server.html',1,'openPubSub::Server'],['../classopenPubSub_1_1Server.html#a630c9fb5fe8495878c414a599a112f5e',1,'openPubSub::Server::Server()']]],
-  ['setnetworkaddressurl_7',['setNetworkAddressUrl',['../classopenPubSub_1_1Server.html#ab9547576369b60e33e527a830554aa82',1,'openPubSub::Server']]],
-  ['settransportprofileuri_8',['setTransportProfileUri',['../classopenPubSub_1_1Server.html#aa7169d5c07db500702a59f510ab9aed3',1,'openPubSub::Server']]],
+  ['setnetworkaddressurl_7',['setNetworkAddressUrl',['../classopenPubSub_1_1Server.html#abe73bfd1d3f2818bda6dddbafbee8b80',1,'openPubSub::Server']]],
+  ['settransportprofileuri_8',['setTransportProfileUri',['../classopenPubSub_1_1Server.html#ac5656ca98a312ca7cb476d98d1920de3',1,'openPubSub::Server']]],
   ['string_9',['string',['../structopenPubSub_1_1string.html',1,'openPubSub']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['server_18',['Server',['../classopenPubSub_1_1Server.html#a630c9fb5fe8495878c414a599a112f5e',1,'openPubSub::Server']]],
-  ['setnetworkaddressurl_19',['setNetworkAddressUrl',['../classopenPubSub_1_1Server.html#ab9547576369b60e33e527a830554aa82',1,'openPubSub::Server']]],
-  ['settransportprofileuri_20',['setTransportProfileUri',['../classopenPubSub_1_1Server.html#aa7169d5c07db500702a59f510ab9aed3',1,'openPubSub::Server']]]
+  ['setnetworkaddressurl_19',['setNetworkAddressUrl',['../classopenPubSub_1_1Server.html#abe73bfd1d3f2818bda6dddbafbee8b80',1,'openPubSub::Server']]],
+  ['settransportprofileuri_20',['setTransportProfileUri',['../classopenPubSub_1_1Server.html#ac5656ca98a312ca7cb476d98d1920de3',1,'openPubSub::Server']]]
 ];
