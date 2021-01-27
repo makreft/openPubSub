21b73ac8a7399bfab1a65075f1483083643e5069
makreft <47369576+makreft@users.noreply.github.com>
2021-01-27 23:15:11 +0000

    Deploying to gh-pages from  @ 328f3189614b8c102f4e6d69835aa03a6d916eb6 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['server_20',['Server',['../classopenPubSub_1_1Server.html',1,'openPubSub::Server'],['../classopenPubSub_1_1Server.html#a7332e823dcca6bfff849c57683da0ac5',1,'openPubSub::Server::Server()']]],
+  ['server_20',['Server',['../classopenPubSub_1_1Server.html',1,'openPubSub::Server'],['../classopenPubSub_1_1Server.html#a7332e823dcca6bfff849c57683da0ac5',1,'openPubSub::Server::Server(void)']]],
   ['setcustomserverconfig_21',['setCustomServerConfig',['../classopenPubSub_1_1Server.html#a4921969b3916a4fe6f53089916cbd5f7',1,'openPubSub::Server']]],
   ['stopserver_22',['stopServer',['../classopenPubSub_1_1Server.html#ab1901c22394e981579fb9f1cd2ed3862',1,'openPubSub::Server']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['adddatasetfield_27',['addDataSetField',['../classopenPubSub_1_1Server.html#a82a48c6d35e74351f649f8b3f53f2516',1,'openPubSub::Server']]],
+  ['adddatasetfield_27',['addDataSetField',['../classopenPubSub_1_1Server.html#a2bcfa28e84ac189e6ef2368659541d94',1,'openPubSub::Server']]],
   ['adddatasetwriter_28',['addDataSetWriter',['../classopenPubSub_1_1Server.html#a873cc8d12b94ec848fb0963be579d37b',1,'openPubSub::Server']]],
-  ['addobjectnode_29',['addObjectNode',['../classopenPubSub_1_1Server.html#a802b7674a049958ee38a8aea466fa9a3',1,'openPubSub::Server']]],
-  ['addpublisheddataset_30',['addPublishedDataSet',['../classopenPubSub_1_1Server.html#a6a21f6e52ec7482600a591786d8afdf6',1,'openPubSub::Server']]],
-  ['addpubsubconnection_31',['addPubSubConnection',['../classopenPubSub_1_1Server.html#a44b8e3effa9e07e9782547355a7931e9',1,'openPubSub::Server']]],
+  ['addobjectnode_29',['addObjectNode',['../classopenPubSub_1_1Server.html#a854a7dd681de75231442ea4d7134c604',1,'openPubSub::Server']]],
+  ['addpublisheddataset_30',['addPublishedDataSet',['../classopenPubSub_1_1Server.html#ae0684e03ba16abf55b219e28e5a5653f',1,'openPubSub::Server']]],
+  ['addpubsubconnection_31',['addPubSubConnection',['../classopenPubSub_1_1Server.html#ac6aa0102c3e3b7073069e8a124023394',1,'openPubSub::Server']]],
   ['addreadergroup_32',['addReaderGroup',['../classopenPubSub_1_1Server.html#aeea91755acf0d26cd309bca99fcc10ba',1,'openPubSub::Server']]],
   ['addvariablenode_33',['addVariableNode',['../classopenPubSub_1_1Server.html#a8d6da253d6d44d6a803f564cb0e89a38',1,'openPubSub::Server::addVariableNode(UA_NodeId variableNodeId, const UA_NodeId folderId, char *nodeDisplayName, char *browseName, UA_Boolean UA_DataType, bool setAccessLevel)'],['../classopenPubSub_1_1Server.html#a76e1efb7b25c1c443e450850016f224f',1,'openPubSub::Server::addVariableNode(UA_NodeId variableNodeId, const UA_NodeId folderId, char *nodeDisplayName, char *browseName, UA_NodeId UA_DataType, bool setAccessLevel)'],['../classopenPubSub_1_1Server.html#a05e7e2497628d76b70ce05d86c77109e',1,'openPubSub::Server::addVariableNode(UA_NodeId variableNodeId, const UA_NodeId folderId, char *nodeDisplayName, char *browseName, UA_DateTime UA_DataType, bool setAccessLevelMask)']]],
   ['addwritergroup_34',['addWriterGroup',['../classopenPubSub_1_1Server.html#ae3f36959117a174daacdb2aeba657fff',1,'openPubSub::Server']]]
