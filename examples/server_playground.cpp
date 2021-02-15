c42f45552b9b396b3d5a5c15d4190b703c91877b
makreft <marco.kreft@tu-bs.de>
2021-02-15 12:38:04 +0100

    adjust test cases

--- examples/server_playground.cpp
+++ examples/server_playground.cpp
@@ -2,6 +2,25 @@
 #include "../openPubSub/ua_pubsub/ua_pubsub.h"
 #include <open62541/server_config_default.h>
 
+UA_UInt32 *valueStore[1];
+static int callback()
+{
+    return 100;
+}
+
+static void valueUpdateCallback(UA_Server *server, void *data)
+{
+    u_int32_t sensorVal;
+    for (int i = 0; i < 1; ++i)
+    {
+        sensorVal = callback();
+        //std::cout << "Inside valueUpdateCallback, sensorVal: " << sensorVal << "\n";
+        if (sensorVal == -1)
+            printf("Issue with sensor.");
+        *valueStore[i] = sensorVal;
+    }
+}
+
 int main()
 {
 
@@ -13,7 +32,10 @@ int main()
     serv.addWriterGroup("Server Playground WriterGroup");
     serv.addDataSetWriter("Server Playground DataSetWriter");
     serv.addDataSetField("Server Playground DataSetField");
-    serv.freezeWriterGroupConfiguration();
+    UA_Server* ua_server = serv.getUAServer();
+    UA_UInt64 callbackId;
+    printf("here");
+    UA_Server_addRepeatedCallback(ua_server, valueUpdateCallback, NULL, 100,&callbackId);
 
     serv.run();
 }
